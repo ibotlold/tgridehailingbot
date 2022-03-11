394617c66e45a5b9e14babc4419d5fa4cc175013
ibotlold <ibotlold@gmail.com>
2022-03-11 13:27:58 +0900

    remove package-lock.json from git repo
    Moved from telegraf-mysql-session to telegraf and mysql2 modules

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
-const Telegraf = require('telegraf')
-const MySQLSession = require('telegraf-session-mysql')
+const { Telegraf } = require('telegraf')
+const mysql = require('mysql2');
 
+// mysql setup
+const database = mysql.createConnection({
+  host: process.env.YKSTEST_DBHOST,
+  user: process.env.YKSTEST_DBUSER,
+  password: process.env.YKSTEST_DBPASS,
+  database: process.env.YKSTEST_DB
+})
+
+//telegraf setup
 const telegraf = new Telegraf(process.env.YKSTEST_TOKEN)
+telegraf.start((ctx) => ctx.reply('Hello'))
+telegraf.command('driver', (ctx) => registerUserAsDriver(ctx))
+telegraf.launch()
+
+//telegraf stop
+process.once('SIGINT', () => stopApp('SIGINT'))
+process.once('SIGTERM', () => stopApp('SIGTERM'))
+
+function registerUserAsDriver(ctx) {
+	
+}
+
+function stopApp(reason) {
+	telegraf.stop(reason)
+	database = null
+}
