f5096dbe597be066621322c1be0be1046011a628
ibotlold <ibotlold@gmail.com>
2022-03-16 00:36:44 +0900

    Encapsulate checks for environment variables

--- build/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const telegraf_develop_1 = require("telegraf-develop");
-const config_1 = require("./config");
-const BOT_TOKEN = config_1.Config.TOKEN;
-if (typeof BOT_TOKEN !== 'string') {
-    throw Error('Telegram bot token not found');
-}
-const bot = new telegraf_develop_1.Telegraf(BOT_TOKEN);
--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,10 @@
-export const Config = {
-    TOKEN: process.env.YKSTEST_TOKEN
+const BOT_TOKEN = process.env.YKSTEST_TOKEN
+
+export class Config {
+    public static TOKEN() {
+        if (typeof BOT_TOKEN !== 'string') {
+            throw Error('Telegram bot token not found')
+        }
+        return BOT_TOKEN
+    }
 }
