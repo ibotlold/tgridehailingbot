8f8c0019e399252470056003d178c385adbd181d
ibotlold <ibotlold@gmail.com>
2022-03-16 01:46:18 +0900

    Added comments

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,16 @@
 const BOT_TOKEN = process.env.YKSTEST_TOKEN
 
+/**
+ * Return config environment variables
+ * @example
+ * Config.TOKEN()
+ */
 export class Config {
+    /**
+     * Solve possible undefined value for process environment variable.
+     * Kill app when env variable is `undefined`.
+     * @returns `string`
+     */
     public static TOKEN() {
         if (typeof BOT_TOKEN !== 'string') {
             console.error('Telegram Bot API authentication token not set')
