3b2ce7df4f4ccd2f537749d0a0cf258d4840a06f
ibotlold <ibotlold@gmail.com>
2022-03-16 00:24:05 +0900

    Export config to separate file

--- build/app.js
+++ build/app.js
@@ -1,2 +1,9 @@
 "use strict";
-console.log('Node.js TypeScript');
+Object.defineProperty(exports, "__esModule", { value: true });
+const telegraf_develop_1 = require("telegraf-develop");
+const config_1 = require("./config");
+const BOT_TOKEN = config_1.Config.TOKEN;
+if (typeof BOT_TOKEN !== 'string') {
+    throw Error('Telegram bot token not found');
+}
+const bot = new telegraf_develop_1.Telegraf(BOT_TOKEN);
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-console.log('Node.js TypeScript');
+import { Telegraf } from 'telegraf-develop'
+import { Config } from './config'
+
+const BOT_TOKEN = Config.TOKEN
+if (typeof BOT_TOKEN !== 'string') {
+    throw Error('Telegram bot token not found')
+}
+
+const bot = new Telegraf(BOT_TOKEN)
