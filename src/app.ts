7294f6898a61dcf7db865448d46441512e677316
ibotlold <ibotlold@gmail.com>
2022-03-16 23:30:25 +0900

    Logs updates

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
-import { Telegraf } from 'telegraf-develop'
+import { Telegraf } from 'telegraf'
 import { Config } from './config'
 
 const BOT_TOKEN = Config.TOKEN()
 
 const bot = new Telegraf(BOT_TOKEN)
+bot.use((ctx) => {
+    console.log('New update')
+    
+    console.log(ctx.update)
+    
+})
+bot.launch()
