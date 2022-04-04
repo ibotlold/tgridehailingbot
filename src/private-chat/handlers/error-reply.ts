8eb992821da923bcd82975fbcb73a2e86579cf39
ibotlold <ibotlold@gmail.com>
2022-04-04 22:00:16 +0900

    Add wrong callback debug info

--- src/private-chat/handlers/error-reply.ts
+++ src/private-chat/handlers/error-reply.ts
@@ -6,6 +6,10 @@ import { supportInlineButton } from "../utils";
 const chat = new Composer()
 chat.on('callback_query', ctx => {
     logger.verbose('User clicked wrong callback keyboard')
+    logger.debug(JSON.stringify({
+        user: ctx.from.id,
+        callback: ctx.callbackQuery.data
+    }))
     ctx.answerCallbackQuery('Ошибка')
     ctx.editMessageText('Воспользуйтесь /start', {
         reply_markup: new InlineKeyboard().row(supportInlineButton)
