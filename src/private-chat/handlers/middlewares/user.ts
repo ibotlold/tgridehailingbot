32764aa1a812ec8e276ab923b38f33a0cf5835ec
ibotlold <ibotlold@gmail.com>
2022-04-08 10:58:19 +0900

    Refactor `user` handler to user dao Changed code formatting

--- src/private-chat/handlers/middlewares/user.ts
+++ src/private-chat/handlers/middlewares/user.ts
@@ -1,20 +1,24 @@
 import { Context, NextFunction } from "grammy";
-import { userDidChangedStatus } from "../../private-chat-controller";
+import User from "../../../dao/user/user";
+import { dao } from "../../private-chat-controller";
 import { logger } from "../../utils";
 
 export default async function user(
     ctx: Context,
     next: NextFunction
-    ):Promise<void> {
-        const newStatus = ctx.update.my_chat_member!.new_chat_member.status
-        logger.info('Changed status', { 
-            user: { 
-                userId: ctx.from!.id,
-                status: newStatus
-            }
-        })
-        await userDidChangedStatus(
-            ctx.from!.id,
-            ctx.update.my_chat_member!.new_chat_member.status
-            )
-        } 
+):Promise<void> {
+    const newStatus = ctx.update.my_chat_member!.new_chat_member.status
+    logger.info('Changed status', { 
+        user: { 
+            userId: ctx.from!.id,
+            status: newStatus
+        }
+    })
+    const user = new User(ctx.from!.id, newStatus)
+    const userFromDB = await dao.userDAO?.findUserById(user.userId)
+    if (userFromDB) {
+        await dao.userDAO?.updateUser(userFromDB, user)
+        return
+    }
+    dao.userDAO?.insertUser(user)
+}
