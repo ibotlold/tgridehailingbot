b723c05990937964505f347c02602484f19cd0c3
ibotlold <ibotlold@gmail.com>
2022-04-14 11:48:40 +0900

    Add DAO objects to `collections`

--- src/database.ts
+++ src/database.ts
@@ -1,12 +1,14 @@
 import * as mongoDB from 'mongodb';
 import { Config } from './config'
 import DriverEntity from './dao/driver/driver-entity';
+import RequestEntity from './dao/request/request-entity'
 import UserEntity from './dao/user/user-entity';
 import mongoDAO from './mongo/dao-impl';
 
 export const collections: {
   users?: mongoDAO<UserEntity>
   drivers?: mongoDAO<DriverEntity>
+  requests?: mongoDAO<RequestEntity>
 } = {}
 
 export async function connectToDatabase() {
@@ -17,4 +19,6 @@ export async function connectToDatabase() {
   collections.users = new mongoDAO(usersCollection)
   const driversCollection = db.collection<DriverEntity>('drivers')
   collections.drivers = new mongoDAO(driversCollection)
+  const requestCollection = db.collection<RequestEntity>('requests')
+  collections.requests = new mongoDAO(requestCollection)
 }
